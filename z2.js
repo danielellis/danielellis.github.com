f6afd31288b38b37429ec954f236d9a4bc4da11d
Daniel Ellis <coug36@gmail.com>
2014-03-22 16:52:21 -0700

    Z2 working in Chrome/Safari

---
+++ z2.js
@@ -0,0 +1,100 @@
+//
+// Arbitrary data structure for menu items. Each menu item contains two
+// properties: text data (expected string) and children (optional array of
+// additional menu items).
+//
+var menuItems = [
+    {
+        text: "Menu one",
+        children: [
+            { text: "Menu one, Item one" },
+            { text: "Menu one, Item two" },
+            { text: "Menu one, Item three" },
+            { text: "Menu one, Item four" },
+            { text: "Menu one, Item five" },
+        ]
+    },
+    {
+        text: "Menu two",
+        children: [
+            { text: "Menu two, Item one" },
+            { text: "Menu two, Item two" },
+            { text: "Menu two, Item three" },
+            { text: "Menu two, Item four" },
+            { text: "Menu two, Item five" },
+        ]
+    },
+    {
+        text: "Menu three",
+        children: [
+            { text: "Menu three, Item one" },
+            { text: "Menu three, Item two" },
+            { text: "Menu three, Item three" },
+            { text: "Menu three, Item four" },
+            { text: "Menu three, Item five" },
+        ]
+    },
+    {
+        text: "Menu four",
+        children: [
+            { text: "Menu four, Item one" },
+            { text: "Menu four, Item two" },
+            { text: "Menu four, Item three" },
+            { text: "Menu four, Item four" },
+            { text: "Menu four, Item five" },
+        ]
+    },
+    {
+        text: "Menu five",
+        children: [
+            { text: "Menu five, Item one" },
+            { text: "Menu five, Item two" },
+            { text: "Menu five, Item three" },
+            { text: "Menu five, Item four" },
+            { text: "Menu five, Item five" }
+        ]
+    },
+];
+
+Element.prototype.show = function() {
+    this.style.display = '';
+};
+
+Element.prototype.hide = function() {
+    this.style.display = 'none';
+};
+
+function buildMenu(container, treeBranch, submenu) {
+    var menuContainer, menuItem;
+    var i;
+
+    menuContainer = document.createElement('div');
+    menuContainer.classList.add('menu-container');
+    if (submenu) {
+        menuContainer.classList.add('submenu');
+    }
+    menuContainer.hide();
+
+    for (i = 0; i < treeBranch.length; i++) {
+        menuItem = document.createElement('div');
+        menuItem.classList.add('menu-item');
+        menuItem.appendChild(document.createTextNode(treeBranch[i].text));
+
+        if (Array.isArray(treeBranch[i].children)) {
+            buildMenu(menuItem, treeBranch[i].children, true);
+        }
+
+        menuContainer.appendChild(menuItem);
+    }
+
+    container.addEventListener('mouseenter', function() {
+        menuContainer.show();
+    });
+    container.addEventListener('mouseleave', function() {
+        menuContainer.hide();
+    });
+
+    container.appendChild(menuContainer);
+}
+
+buildMenu(document.getElementById('menu-button'), menuItems);
